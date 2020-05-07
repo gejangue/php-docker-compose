e2c346f9671ad1b1684c676ccff37736b74789d7
gejangue <64191861+gejangue@users.noreply.github.com>
2020-05-07 03:34:05 +0200

    echo

--- php/index.php
+++ php/index.php
@@ -23,7 +23,7 @@ $conn = new mysqli($host,$user,$password,$db);
  if($conn->connect_error){
   echo 'connection failed'. $conn->connect_error;
  }
- echo '<br>'
+ echo '<br>';
 echo 'successfully connected to MYSQL';
 
 //$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
